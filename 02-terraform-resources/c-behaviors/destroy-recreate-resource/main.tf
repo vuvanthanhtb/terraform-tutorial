4d7e138bf2bf355b8180a7e881ce2ec0002acea6
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-04-17 21:45:04 +0700

    day two

--- 02-terraform-resources/c-behaviors/destroy-recreate-resource/main.tf
+++ 02-terraform-resources/c-behaviors/destroy-recreate-resource/main.tf
@@ -13,10 +13,10 @@ provider "aws" {
 
 resource "aws_instance" "myEC2" {
   # ami             = "ami-01938df366ac2d954"
-  ami           = "ami-065a492fef70f84b1"
-  instance_type = "t2.micro"
-  key_name      = "terraform-study-key-pair"
-  subnet_id     = "subnet-0f40b534ab32974b9"
+  ami             = "ami-065a492fef70f84b1"
+  instance_type   = "t2.micro"
+  key_name        = "terraform-study-key-pair"
+  security_groups = ["default"]
   tags = {
     Name = "destroyRecreateResource"
   }
